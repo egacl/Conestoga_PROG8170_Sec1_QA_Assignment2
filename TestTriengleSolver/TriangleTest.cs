664b2842a53089a0c01c7ddb3955885055a275f7
Eugenio Contreras <Econtreraslizan1632@conestogac.on.ca>
2023-06-04 00:47:13 -0400

    Second commit with isosceles triangle unit tests

--- TestTriengleSolver/TriangleTest.cs
+++ TestTriengleSolver/TriangleTest.cs
@@ -6,6 +6,10 @@ namespace TestTriengleSolver
     [TestFixture]
     public class TriangleTest
     {
+        private static string VALID_EQUIRATERAL_RESPONSE = "An EQUILATERAL triangle is formed";
+
+        private static string VALID_ISOSCELES_RESPONSE = "An ISOSCELES triangle is formed";
+
 
         [Test]
         public void ValidEquilateral_Input60and60and60_OutputValidTriangle()
@@ -14,7 +18,55 @@ namespace TestTriengleSolver
             int firstSide = 60;
             int secondSide = 60;
             int thirdSide = 60;
-            string expectedResponse = "An EQUILATERAL triangle is formed";
+            string expectedResponse = VALID_EQUIRATERAL_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidIsosceles_Input60and60and40_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 40;
+            string expectedResponse = VALID_ISOSCELES_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidIsosceles_Input40and60and40_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 40;
+            int secondSide = 60;
+            int thirdSide = 40;
+            string expectedResponse = VALID_ISOSCELES_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidIsosceles_Input70and40and40_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 70;
+            int secondSide = 40;
+            int thirdSide = 40;
+            string expectedResponse = VALID_ISOSCELES_RESPONSE;
 
             // Test execution
             string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
