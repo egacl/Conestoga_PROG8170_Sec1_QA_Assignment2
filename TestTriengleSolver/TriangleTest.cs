2115efdfa4044ec832a6933eb6c8355016fc8bbb
Eugenio Contreras <Econtreraslizan1632@conestogac.on.ca>
2023-06-04 00:39:19 -0400

    First commit with equilateral triangle unit test

---
+++ TestTriengleSolver/TriangleTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TriangleSolver;
+
+namespace TestTriengleSolver
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+
+        [Test]
+        public void ValidEquilateral_Input60and60and60_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 60;
+            string expectedResponse = "An EQUILATERAL triangle is formed";
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+    }
+}
