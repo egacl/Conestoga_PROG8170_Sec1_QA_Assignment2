f8c0d195f0180d517b31538a6dc63ed79285ca21
Eugenio Contreras <Econtreraslizan1632@conestogac.on.ca>
2023-06-04 00:57:03 -0400

    Third commit with scalene triangle unit tests

--- TestTriengleSolver/TriangleTest.cs
+++ TestTriengleSolver/TriangleTest.cs
@@ -10,6 +10,8 @@ namespace TestTriengleSolver
 
         private static string VALID_ISOSCELES_RESPONSE = "An ISOSCELES triangle is formed";
 
+        private static string VALID_SCALENE_RESPONSE = "A SCALENE triangle is formed";
+
 
         [Test]
         public void ValidEquilateral_Input60and60and60_OutputValidTriangle()
@@ -67,6 +69,86 @@ namespace TestTriengleSolver
             int secondSide = 40;
             int thirdSide = 40;
             string expectedResponse = VALID_ISOSCELES_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidScalene_Input70and60and50_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 70;
+            int secondSide = 60;
+            int thirdSide = 50;
+            string expectedResponse = VALID_SCALENE_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidScalene_Input60and70and50_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 60;
+            int secondSide = 70;
+            int thirdSide = 50;
+            string expectedResponse = VALID_SCALENE_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidScalene_Input60and50and70_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 60;
+            int secondSide = 50;
+            int thirdSide = 70;
+            string expectedResponse = VALID_SCALENE_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidScalene_Input2and3and4_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 2;
+            int secondSide = 3;
+            int thirdSide = 4;
+            string expectedResponse = VALID_SCALENE_RESPONSE;
+
+            // Test execution
+            string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expectedResponse, executionResponse);
+        }
+
+        [Test]
+        public void ValidScalene_Input4and3and5_OutputValidTriangle()
+        {
+            // Test Data Preparation
+            int firstSide = 4;
+            int secondSide = 3;
+            int thirdSide = 5;
+            string expectedResponse = VALID_SCALENE_RESPONSE;
 
             // Test execution
             string executionResponse = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
